8c70cfbb85e35d4b029cad73f6e11cee18e3a7d0
root <root@ip-172-31-47-139.ap-south-1.compute.internal>
2020-12-05 16:45:54 +0000

    added path's

--- Project-1/Tomcat/tomcat.sh
+++ Project-1/Tomcat/tomcat.sh
@@ -8,5 +8,9 @@ echo "Extract Tomcat file"
 tar xvf apache-tomcat-8.5.60.tar.gz
 echo "Change Directory name"
 mv apache-tomcat-8.5.60 tomcat-8
+cp /root/CICID-projects/Project-1/Tomcat/context.xml /opt/tomcat-8/webapps/manager/META-INF/
+cp /root/CICID-projects/Project-1/Tomcat/context.xml /opt/tomcat-8/webapps/host-manager/META-INF/
+cp /root/CICID-projects/Project-1/Tomcat/tomcat-users.xml /opt/tomcat-8/conf/
 echo "Start Tomcat server"
 bash /opt/tomcat-8/bin/startup.sh
+
